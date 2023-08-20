bb7522c0a3495ec8d4218b2b8dc18b9a8c02d1de
Artur Smirnov <artursmirnovserg@gmail.com>
2023-08-21 02:57:17 +0800

    upd FormulaError

--- antlr/structures.cpp
+++ antlr/structures.cpp
@@ -76,3 +76,32 @@ Position Position::FromString(std::string_view str) {
 bool Size::operator==(Size rhs) const {
     return cols == rhs.cols && rows == rhs.rows;
 }
+
+std::ostream& operator<<(std::ostream& output, FormulaError fe) {
+
+}
+
+FormulaError::FormulaError(Category category)
+    : category_(category)
+{}
+
+FormulaError::Category FormulaError::GetCategory() const {
+    return category_;
+}
+
+bool FormulaError::operator==(FormulaError rhs) const {
+    return category_ == rhs.category_;
+}
+
+std::string_view FormulaError::ToString() const {
+    switch (category_) {
+    case Category::Ref:
+        return "#REF!";
+    case Category::Value:
+        return "#VALUE!";
+    case Category::Div0:
+        return "#DIV/0!";
+    default:
+        return "";
+    }
+}
